730cf3f6c3601b4936dc3fea4802e39b9e558f5a
Dinh Thanh Huyen <dtth247vp@gmail.com>
2022-08-06 11:09:11 +0700

    mini app

---
+++ app/src/main/java/com/dtth/myapplication/models/Player.java
@@ -0,0 +1,15 @@
+package com.dtth.myapplication.models;
+
+public class Player {
+    public String name;
+    public int score = 0;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
